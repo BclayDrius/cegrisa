ba44d81c2d714ce63953e67c27e4fb14201ca69a
Arian Liendro <arian2005lc@gmail.com>
2025-10-22 22:04:05 -0500

    s

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -5,7 +5,7 @@ export default function Categories() {
   const navigate = useNavigate();
 
   const cards = [
-    { title: "Porcelanato", page: "/Documents", img: "https://i.pinimg.com/1200x/fa/2f/b0/fa2fb0e59ef65d90b41778642218a1a0.jpg" },
+    { title: "Porcelanatos", page: "/Documents", img: "https://i.pinimg.com/1200x/fa/2f/b0/fa2fb0e59ef65d90b41778642218a1a0.jpg" },
     { title: "Sanitarios", page: "/Documents", img: "https://i.pinimg.com/736x/3b/a3/6e/3ba36e39f0ca0a0104d45bdaf674cc65.jpg" },
     { title: "Tablones", page: "/Products", img: "https://i.pinimg.com/736x/ac/01/15/ac0115c4b587cb54d0241117e8c6573d.jpg" },
     { title: "Sinterizados", page: "/Products", img: "https://i.pinimg.com/1200x/79/eb/5a/79eb5ac9899b960b1e822dbce19dc73e.jpg" },
